4cc23015fbface93151d025b214b2ca82a8623c6
raunak_k02 <raunakk728@gmail.com>
2023-07-01 21:15:12 +0530

    added homepage

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -22,7 +22,52 @@ function HomePage() {
   }, []);
 
   return (
-      <Navigate to={'signup'} />
+      // <Navigate to={'signup'} />
+      <div>
+        <header>
+          <nav>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Recipes</a></li>
+              <li><a href="#">Tips &amp; Tricks</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </nav>
+        </header>
+        
+        <section className="hero">
+          <div className="hero-content">
+            <h1>Reduce Food Waste, Save the Planet</h1>
+            <p>Discover easy and practical ways to reduce food waste in your kitchen</p>
+            <a href="#" className="btn">Get Started</a>
+          </div>
+        </section>
+        
+        <section className="features">
+          <div className="feature">
+            <img src="https://picsum.photos/40/40" alt="Icon 1" />
+            <h2>Recipe Suggestions</h2>
+            <p>Explore delicious recipes that use ingredients you already have to minimize waste.</p>
+          </div>
+          
+          <div className="feature">
+            <img src="https://picsum.photos/40/40" alt="Icon 2" />
+            <h2>Tips &amp; Tricks</h2>
+            <p>Learn useful tips and tricks to extend the shelf life of your groceries and avoid waste.</p>
+          </div>
+          
+          <div className="feature">
+            <img src="https://picsum.photos/40/40" alt="Icon 3" />
+            <h2>Community</h2>
+            <p>Connect with like-minded individuals who are passionate about reducing food waste.</p>
+          </div>
+        </section>
+        
+        <footer>
+          <p>&copy; 2023 Food Waste Reduction App. All rights reserved.</p>
+        </footer>
+      </div>
   );
 }
 
